c058c5afa798b9258f698373e15df933fafddcf7
Anett Gesierich <emsuiko@gmail.com>
2018-06-06 22:20:10 +0200

    add history

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# doodleCSS-what?
+# CSSdoo-what?
 
 Remember passing along notes in class? Sometimes just doodling down something and the friend next to you added to it and the one next to them etc.pp. and eventually a nice little fun piece of doodle art was created.
 CSSDoodle is just that, well... a little more tech-y maybe. ;)
+
+Here's the current doodle: [https://emsuiko.github.io/CSSdoodle/](https://emsuiko.github.io/CSSdoodle/)
 
 # How to contribute
 
@@ -9,25 +11,53 @@ CSSDoodle is just that, well... a little more tech-y maybe. ;)
 
 This project uses [pug](https://pugjs.org/api/getting-started.html) for templating and [SASS](https://sass-lang.com/) for styling.
 
-1. fork this repository
-2. open terminal and run `npm install`
-3. run `gulp tile` will create your personal tile files. The name is a random string. The name of your tile (and hence the name of your pug and sass) is logged on the console. You can find your tile template in src/pug/tiles and your style file in src/sass/tiles. They are automatically added to index.pug and styles.sass
-4. run `gulp` to build the current state of the doodle. A new browser sync session will open in your browser via `localhost:3000`.
+1. fork and clone this repository
+2. open terminal, navigate to your project repository and run `npm install --production`
+3. run `gulp tile` to create your personal tile files.  
+The name of your tile (and hence the name of your pug and sass) is a random string and will be logged on the console.  
+You can find your tile template in `src/pug/tiles` and your style file in `src/sass/tiles`. They are automatically added to `index.pug` and `styles.sass`
+4. run `gulp watch` to build the current state of the doodle. A new browser sync session will open in your browser via `localhost:3000`.
 5. open your favourite editor or IDE and start coding. The pug and sass files will be automatically processed while gulp (watch) is still running in your terminal; browser sync makes sure to refresh your page automatically too.
-6. open a pull request.
+6. when you are satisfied with your doodle, open a pull request.
 
 ## Guidlines
 Some guidelines for your doodle...
 
 ### Content
-It's entirely up to you what you are creating. Please remember: be kind and have fun!
+It's entirely up to you what you are creating. Please remember: be kind and have fun!  
 You can place your tile anywhere on the canvas but please don't overlap other CSS-artists tiles (entirely). "Interacting" with other commponents is fine though.
 Please refrain from using words or depiction of sexism, racism, hate or anything along these lines - on your tile and in your code too!
 
 ### Development
 - The canvas has a fixed size of 1440x900px - please don't change this.
-- Every tile is initially set to a fixed size of 100x100px - you can change this for your tile if you like. Remember: This is a community thingy, please don't use too much space.
+- Every tile is initially set to a fixed size of 100x100px - you can change this for your tile if you like. Remember: This is a community thingy, please don't use up too much space.
+- The tiles are placed with `position: absolute`, relative to the canvas. You can move them inside the canvas with the top/bottom/right/left properties.
 - You can add as much or as little elements to your tile as you like. It doesn't have to be just a single div image.
 - HTML (pug) and CSS (sass) only please! :)
-- add your IDE/editor specific configuration to .gitignore if necessary
+- add your IDE/editor specific configuration to `.gitignore` if necessary
 
+## History
+
+The evolution of the canvas is tracked via screenshots on the history page. (I'll add your line after merging your pull request.)
+
+You can add it yourself if you like, though.
+
+Here's how...
+### Prerequisites
+You need to have installed following tools on your computer:
+
+For taking the screenshot the package [gulp-webshot](https://www.npmjs.com/package/gulp-webshot) is used.  
+It needs:
+- [phantomjs](http://phantomjs.org/)
+
+For creating the thumbnail the package [gulp-image-resize](https://www.npmjs.com/package/gulp-image-resize) is used.  
+It needs:
+- imagemagick
+- graphicsmagick
+
+phantomjs, imagemagick and graphicsmagick won't be installed through npm!
+
+### Add your history entry
+1. open terminal, navigate to your project repository and run `npm install` to install devDependencies w/ aforementioned packages.
+2. run `gulp shot` to take the screenshot and generate the thumbnail. The filenames match your tilename.
+3. run `gulp history` with your github account name as `name` parameter, e.g. `gulp history --name emsuiko`, to add a new row in the `pug/history`. 
--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,10 @@ var plumber = require('gulp-plumber');
 
 var randomize = require('randomatic');
 
+var fs = require('fs');
+var path = require('path');
+var _ = require('underscore');
+
 var env = require('minimist')(process.argv.slice(2))._[0] || 'development';
 
 var isDevelopment = function() {
@@ -55,6 +59,11 @@ function templates(filestream) {
         .pipe($.if(isDevelopment(),browserSync.stream()));
 }
 
+function imagine(file) {
+    return gulp.src([file])
+        .pipe(gulp.dest('dist/img'));
+}
+
 function newTileSass(name) {
     var str = 'div.'+name;
     return $.file(name+'.sass', str, { src: true }).pipe(gulp.dest('src/sass/tiles')) &&
@@ -74,39 +83,82 @@ function newTilePug(name) {
         gulp
             .src('src/pug/index.pug')
             .pipe($.modifyFile((content, path, file) => {
-                const add = '\n    include tiles/'+name;
+                const add = '\n            include tiles/'+name;
 
                 return `${content}${add}`;
             }))
             .pipe(gulp.dest('src/pug'));
 }
 
+// source: https://stackoverflow.com/questions/15696218/get-the-most-recent-file-in-a-directory-node-js
+function getMostRecentFileName(dir) {
+    var files = fs.readdirSync(dir);
+
+    // use underscore for max()
+    return _.max(files, function (f) {
+        var fullpath = path.join(dir, f);
+
+        // ctime = creation time is used
+        // replace with mtime for modification time
+        return fs.statSync(fullpath).ctime;
+    }).slice(1, -4);
+}
+
+function takeScreenshot(filename) {
+    return gulp.src('./dist/index.html')
+        .pipe($.webshot({
+                    dest:'./src/img',
+                    root:'./dist',
+                    screenSize: { width: 1600, height: 1000 },
+                    shotSize: {width: 1600, height: 1000 },
+                    filename: filename
+                })
+        );
+}
+
+function createThumbnail(filename) {
+    return gulp.src('src/img/'+filename+'.png')
+            .pipe($.imageResize({
+              width : 200,
+              height : 125,
+              crop : true,
+              upscale : false
+            }))
+            .pipe(gulp.dest('src/img/thumb'));
+}
+
 // * * * * * TASKS * * * * * //
 
 gulp.task('templates', function() {
-    return templates(gulp.src('src/pug/index.pug'));
+    return templates(gulp.src(['src/pug/*.pug', '!src/pug/_*.pug']));
 });
 
 gulp.task('sass', function() {
-    return sass(gulp.src('src/sass/**/*.sass'));
+    return sass(gulp.src('src/sass/styles.sass'));
+});
+
+gulp.task('img', function() {
+    return imagine('src/img/**/*');
 });
 
 gulp.task('clean', function() {
     log('Clean processed output.')
     return del([
-        'index.html',
-        'styles.css',
+        'dist/*.html',
+        'dist/*.css',
+        'dist/img/**/*',
+        '!dist/img/.gitkeep',
     ]);
 });
 
-gulp.task('build', ['templates', 'sass']);
+gulp.task('build', ['templates', 'sass', 'img']);
 
-gulp.task('watch', ['templates', 'sass', 'browserSync'], function() {
+gulp.task('watch', ['templates', 'sass', 'img', 'browserSync'], function() {
     log('Start watching...');
 
     watch('src/pug/**/*.pug', { verbose: true }, function(vinyl) {
         var filename = vinyl.path.replace(vinyl.cwd + '/', '');
-        templates(gulp.src('src/pug/index.pug')
+        templates(gulp.src(filename)
                 .pipe(plumber({ errorHandler: onError })),'')
             .on('end', function() {
                 log('...re-pugged '+filename);
@@ -123,6 +175,22 @@ gulp.task('watch', ['templates', 'sass', 'browserSync'], function() {
                 browserSync.reload;
             });
     });
+
+    watch('img/**/*.*', { verbose: true }, function(vinyl) {
+        var filename = vinyl.path.replace(vinyl.cwd + '/', '');
+
+        if (vinyl.event == 'unlink') {
+            del('../dist/img/' + filename, {
+                force: true
+            });
+            log('deleted image: ' + filename);
+        } else {
+            imagine(filename)
+                .on('end', function() {
+                    log('🖼 …re-imagined');
+                });
+        }
+    });
 });
 
 gulp.task('browserSync', function() {
@@ -135,18 +203,34 @@ gulp.task('browserSync', function() {
 
 gulp.task('tile', function() {
     var name = randomize('Aa0', 10);
-    newTilePug(name);
-    newTileSass(name);
+    var filename = '_'+name;
+    newTilePug(filename);
+    newTileSass(filename);
     log('Congratulations! You just created a new tile for doodling: '+name);
 });
 
-gulp.task('screens', function () {
-  gulp.src('index.html')
-  .pipe($.localScreenshots({
-    width: ['1600']
-   }))
-  .pipe(gulp.dest('.'));
-});
+gulp.task('shot', ['build'], function() {
+    var filename = getMostRecentFileName("src/pug/tiles");
+    log('Creating screenshot and thumbnail for tile: '+filename);
+    return takeScreenshot(filename) && createThumbnail(filename);
+})
+
+gulp.task('history', function(){
+    var name = require('minimist')(process.argv)['name'];
+    var filename = getMostRecentFileName("src/pug/tiles");
+    return gulp.src(['src/pug/history/_blank.pug'])
+        .pipe($.rename('_'+filename+'.pug'))
+        .pipe($.replace('filename', filename))
+        .pipe($.replace('yournamehere', name))
+        .pipe(gulp.dest('src/pug/history/'))
+        && gulp.src('src/pug/history.pug')
+        .pipe($.modifyFile((content, path, file) => {
+            const add = '\n                include history/_'+filename;
+
+            return `${content}${add}`;
+        }))
+        .pipe(gulp.dest('src/pug'));
+  });
 
 gulp.task('default', ['clean'], function() {
     log('Start build for ' + env);
